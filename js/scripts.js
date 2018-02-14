c4de1a60652dba49510b42967cde6cacb65e2486
Deep Parmar <deep98parmar@yahoo.com>
2018-02-14 12:53:28 +0300

    work

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("p").click(function() {
 
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").fadeToggle();
+    $("#walrus-hidden").fadeToggle();
     });
     });
